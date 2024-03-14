b44317215b231d69242d918ecf193914dede9b8a
athena-dai <dai.athena@gmail.com>
2024-03-14 18:08:00 -0400

    completed lab

--- load_model.py
+++ load_model.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 
 # TODO: Set tht MLFlow server uri
-uri = ___________________
+uri = "http://127.0.0.1:6001"
 mlflow.set_tracking_uri(uri=uri)
 
 # TODO: Provide model path/url
-logged_model = ___________________
+logged_model = "runs:/80afb68acfab4cc9b6f389bfe728ba32/iris_model"
 
 # Load model as a PyFuncModel.
 loaded_model = mlflow.sklearn.load_model(logged_model)
@@ -15,9 +15,12 @@ loaded_model = mlflow.sklearn.load_model(logged_model)
 # Input a random datapoint
 data=np.array([[1.0,2.0,3.0,4.0]])
 
+
 # TODO: Predict on a Pandas DataFrame. Due to the MLFlow functionality constrain.
 #       The loaded model's predict function only accept dataframe as input instead of numpy array.
-prediction=loaded_model.predict(___________)
+data_df = pd.DataFrame(data)
+
+prediction=loaded_model.predict(data_df)
 
 # Print out prediction result
 print(prediction)
